56b8a89075e659fd4a3fd66904939cf201e674fe
MedCheryak <medalicheryak2017@gmail.com>
2023-06-24 20:02:45 +0200

    first commit

---
+++ src/app/services/element.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class ElementService {
+  constructor(private httpClient: HttpClient) {}
+  getElements(): Observable<any> {
+    return this.httpClient.get<any>(
+      `${environment.apiUrl}/${environment.prefix}/elements`
+    );
+  }
+
+  postElement(data: any): Observable<any> {
+    return this.httpClient.post<any>(
+      `${environment.apiUrl}/${environment.prefix}/elements`,
+      data
+    );
+  }
+
+  getElement(id: number): Observable<any> {
+    return this.httpClient.get<any>(
+      `${environment.apiUrl}/${environment.prefix}/elements/${id}`
+    );
+  }
+
+  updateElement(id: number, data: any): Observable<any[]> {
+    return this.httpClient.put<any[]>(
+      `${environment.apiUrl}/${environment.prefix}/elements/${id}`,
+      {
+        id: data.id,
+        name: data.name ?? '',
+        projectId: Number(data.projectId) || 0,
+      }
+    );
+  }
+
+  deleteElement(id: number): Observable<any> {
+    return this.httpClient.delete<any[]>(
+      `${environment.apiUrl}/${environment.prefix}/elements/${id}`
+    );
+  }
+
+  getListProjects(): Observable<any> {
+    return this.httpClient.get<any>(
+      `${environment.apiUrl}/${environment.prefix}/projects`
+    );
+  }
+}
